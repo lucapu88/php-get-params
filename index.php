3b6ff2d86ec2694ebf83aa98ee83f605c4c3fc93
lucapu88 <caputoluca88@gmail.com>
2020-01-20 15:41:36 +0100

    fatto qualcosa

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!-- Creare una variabile che contiene del testo. Leggere dal parametro in GET una badword da cercare all'interno del testo e censurarla, ossia visualizzare a schermo un paragrafo con all'interno il testo in cui le occorrenze della badword sono sostituite da tre "*". -->
+
 <?php
-  echo "inzio";
- ?>
+  // Creare una variabile che contiene del testo
+  $frase = 'Lorem ipsum dolor sit amet. Duis aute irure dolor in reprehenderit in voluptate.';
+  // Leggere dal parametro in GET una badword da cercare all'interno del testo
+  $badword = $_GET['dolor'];
+  // visualizzare a schermo un paragrafo con all'interno il testo in cui le occorrenze della badword sono sostituite da tre "*"
+  $sostituisci = str_replace($badword, '***', $frase);
+  echo $sostituisci;
+
+
+?>
