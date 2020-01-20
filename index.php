3a1d54ea84f972a511a1385245c9350ff4923958
lucapu88 <caputoluca88@gmail.com>
2020-01-20 16:11:35 +0100

    modifiche. fine

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 
 <?php
   // Leggere dal parametro in GET una badword da cercare all'interno del testo
-  $badword = $_GET['testo'];
+  $badword = $_GET['censura_parola'];
   // Creare una variabile che contiene del testo
-  $frase = 'Lorem ipsum dolor sit amet. Duis aute irure dolor in reprehenderit in voluptate.';
-
+  $frase = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   // visualizzare a schermo un paragrafo con all'interno il testo in cui le occorrenze della badword sono sostituite da tre "*"
   $sostituisci = str_replace($badword, '***', $frase);
   print_r($sostituisci);
